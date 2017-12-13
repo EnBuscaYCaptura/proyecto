de3907c298677bedec02fe53a2b4534af35f2914
Cristian <cristian_92ssr@hotmail.com>
2017-12-13 15:43:06 +0100

    correccion en el acceso

--- EnBuscaYCaptura/client/js/vistaCuenta.js
+++ EnBuscaYCaptura/client/js/vistaCuenta.js
@@ -1,6 +1,12 @@
-import { Accounts } from 'meteor/accounts-base';
-import { Meteor } from 'meteor/meteor';
-import { Bert } from 'meteor/themeteorchef:bert';
+import {
+    Accounts
+} from 'meteor/accounts-base';
+import {
+    Meteor
+} from 'meteor/meteor';
+import {
+    Bert
+} from 'meteor/themeteorchef:bert';
 
 
 Template.home.events({
@@ -16,34 +22,41 @@ Template.registro.events({
         var contrasena = event.target.contrasena.value;
         var nombreAcceso = event.target.nombreAcceso.value;
         var nombreUsuario = event.target.nombreUsuario.value;
-        var usuarioApp = {email:email,profile:{nombreUsuario:nombreUsuario},username:nombreAcceso,password:contrasena};
-        if((!email || email.length === 0) || (!contrasena ||  contrasena.length === 0) || (!nombreAcceso || nombreAcceso.length === 0) || (!nombreUsuario || nombreUsuario.length === 0)){
-            Bert.alert( 'Es necesario rellenar todos los campos', 'danger' );
+        var usuarioApp = {
+            email: email,
+            profile: {
+                nombreUsuario: nombreUsuario
+            },
+            username: nombreAcceso,
+            password: contrasena
+        };
+        if ((!email || email.length === 0) || (!contrasena || contrasena.length === 0) || (!nombreAcceso || nombreAcceso.length === 0) || (!nombreUsuario || nombreUsuario.length === 0)) {
+            Bert.alert('Es necesario rellenar todos los campos', 'danger');
         } else {
-            if(nombreAcceso.indexOf('@') === -1) {
-                Accounts.createUser(usuarioApp,function(err){
-                if (err ) {
-                    Bert.alert( err.reason, 'danger' );
-                  } else {
-                    Meteor.call( 'sendVerificationLink', ( err, response ) => {
-                      if ( err ) {
-                        Bert.alert( error.reason, 'danger' );
-                      } else {
-                        Bert.alert({
-                        hideDelay: 9000,
-                        message: 'Correo de verificación enviado',
-                        type: 'success'
+            if (nombreAcceso.indexOf('@') === -1) {
+                Accounts.createUser(usuarioApp, function(err) {
+                    if (err) {
+                        Bert.alert(err.reason, 'danger');
+                    } else {
+                        Meteor.call('sendVerificationLink', (err, response) => {
+                            if (err) {
+                                Bert.alert(error.reason, 'danger');
+                            } else {
+                                Bert.alert({
+                                    hideDelay: 9000,
+                                    message: 'Correo de verificación enviado',
+                                    type: 'success'
+                                });
+                            }
                         });
-                      }
-                    });
-                  }
-                  Meteor.logout();
-                  Router.go('/');
+                    }
+                    Meteor.logout();
+                    Router.go('/');
                 });
             } else {
-                Bert.alert( 'El nombre de acceso a la App no debe contener el caracter @', 'danger' );
+                Bert.alert('El nombre de acceso a la App no debe contener el caracter @', 'danger');
             }
-            
+
         }
     }
 });
@@ -54,13 +67,17 @@ Template.acceso.events({
         var nombreAcceso = event.target.nombreAcceso.value;
         var contrasena = event.target.contrasena.value;
         if (nombreAcceso || contrasena) {
-            Meteor.loginWithPassword(nombreAcceso, contrasena, function(err) {
-                if (err) {
-                    Bert.alert( 'Por favor, revise todos los campos', 'danger' );
-                } else {
-                    Router.go('/');
-                }
-            });
+            if (nombreAcceso.indexOf('@') === -1) {
+                Meteor.loginWithPassword(nombreAcceso, contrasena, function(err) {
+                    if (err) {
+                        Bert.alert('Por favor, revise todos los campos', 'danger');
+                    } else {
+                        Router.go('/');
+                    }
+                });
+            } else {
+                Bert.alert('Se accede con el nombre, no con el correo', 'danger');
+            }
         } else {
             Bert.alert({
                 message: 'Debes completar todos los campos',
@@ -101,13 +118,13 @@ Template.modificarUsuario.events({
             Meteor.call('eliminarEmail', emailAntiguo)
             Meteor.call('anadirEmail', emailMod, function(err) {
                 if (!err) {
-                  Bert.alert({
-                  hideDelay: 6000,
-                  message: 'Recuerde verificar el nuevo email para seguir usando la aplicación',
-                  type: 'info',
-                  icon: 'fa-envelope-o'
-                  });
-                  Router.go('/listarTesoros');
+                    Bert.alert({
+                        hideDelay: 6000,
+                        message: 'Recuerde verificar el nuevo email para seguir usando la aplicación',
+                        type: 'info',
+                        icon: 'fa-envelope-o'
+                    });
+                    Router.go('/listarTesoros');
 
                 }
             });
@@ -129,21 +146,21 @@ Template.modificarUsuario.events({
         }
         Bert.alert('Datos modificados', 'success');
     },
-    
-    'click .eliminar-cuenta': function () {
-        Meteor.call('eliminarUsuario', function(err){
-            if(!err) {
+
+    'click .eliminar-cuenta': function() {
+        Meteor.call('eliminarUsuario', function(err) {
+            if (!err) {
                 Bert.alert({
-                  hideDelay: 6000,
-                  message: 'Hasta pronto, camarada',
-                  type: 'info',
-                  icon: 'fa-remove'
-                  });
+                    hideDelay: 6000,
+                    message: 'Hasta pronto, camarada',
+                    type: 'info',
+                    icon: 'fa-remove'
+                });
                 Router.go('/');
             }
         });
     },
-        'click .btn-logout': function() {
+    'click .btn-logout': function() {
         Meteor.logout();
         Router.go('/');
     }
